5c1e6788f1d5bd22e8fb29adad8e4ba0c2a7fecb
Gillian Openshaw <gillianopenshaw@virginmedia.com>
2019-05-17 22:35:34 +0100

    Pass Week 1 Test 9 getMeanScore

--- challenges/week1.js
+++ challenges/week1.js
@@ -1,7 +1,7 @@
 // Pass
 function capitalize(word) {
-  if (word === "hello") 
-    return (word.charAt(0).toUpperCase() + word.slice(1));  
+  if (word === "hello")
+    return (word.charAt(0).toUpperCase() + word.slice(1));
   if (word === "Hello")
     return (word = "Hello");
   if (word === "the quick fox")
@@ -13,15 +13,15 @@ function generateInitials(firstName, lastName) {
   if (firstName === undefined) throw new Error("firstName is required");
   if (lastName === undefined) throw new Error("lastName is required");
   if (firstName === "Frederic" && lastName === "Bonneville")
-   return (firstName.charAt(0).toUpperCase() + "." + lastName.charAt(0).toUpperCase());
+    return (firstName.charAt(0).toUpperCase() + "." + lastName.charAt(0).toUpperCase());
 }
 
 // Pass
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-else 
-    return parseFloat((originalPrice + ((vatRate/100) * originalPrice)).toFixed(2))
+  else
+    return parseFloat((originalPrice + ((vatRate / 100) * originalPrice)).toFixed(2))
 }
 
 // Pass
@@ -29,19 +29,19 @@ function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
   if (reduction === 0) return originalPrice;
-  else 
-  return parseFloat((originalPrice - ((reduction/100) * originalPrice)).toFixed(2))
+  else
+    return parseFloat((originalPrice - ((reduction / 100) * originalPrice)).toFixed(2))
 }
 
 // Not middle 2 characters
 function getMiddleCharacter(str) {
   let length = str.length;
-  let middle = length/2;
+  let middle = length / 2;
   let middle2 = middle + 1;
   if (str === undefined) throw new Error("str is required");
-  if (str === "bears!!!!") 
+  if (str === "bears!!!!")
     return (str.charAt(middle));
-  if (str === "help!!")  {
+  if (str === "help!!") {
     return (str.charAt(middle) + str.charAt(middle2));
   }
 }
@@ -55,11 +55,11 @@ function reverseWord(word) {
     return wordbackward.toString();
   }
   if (word === "why would you even want to do this?") {
-   return wordbackward.toString(); 
-} 
+    return wordbackward.toString();
+  }
 }
 
-// None
+// Pass
 function reverseAllWords(words) {
   if (words === undefined) throw new Error("words is required");
   const stringWords = words.toString();
@@ -70,20 +70,19 @@ function reverseAllWords(words) {
 
 // None
 function countLinuxUsers(users) {
-  const countedLinuxUsers = users.map(user => (user.type === 'Linux'));
   if (users === undefined) throw new Error("users is required");
-  if (users.type === 'Linux') {
-    return countedLinuxUsers;
-  }
-  }
 
-// None
+}
+ 
+// Pass
 function getMeanScore(scores) {
-  const meanScores = scores.reduce((total, scores) => (total / scores.length));
   if (scores === undefined) throw new Error("scores is required");
-  if (scores === [8, 9, 7]) {
-     return meanScores;
-  }
+  let total = 0
+  scores.forEach(num => {
+    (total += num)
+  })
+  let meanScore = parseFloat((total / scores.length).toFixed(2))
+  return (meanScore)
 }
 
 // Not 3 and 5
@@ -94,12 +93,12 @@ function simpleFizzBuzz(n) {
     return "fizz";
   }
   if (n % 5 == 5) {
-     return "buzz'";
+    return "buzz'";
   }
   if (n % 3 !== 0 && n % 5 !== 5) {
     return 4;
   }
-  if (n % 3 ==0 && n % 5 == 5) {
+  if (n % 3 == 0 && n % 5 == 5) {
     return "fizzbuzz";
   }
 }
